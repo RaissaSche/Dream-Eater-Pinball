9d47d5411ff5f6fe182a6ad5d6b317244d74d720
Raissa Scheeren <raissa.scheeren@cwi.com.br>
2018-12-09 15:31:57 -0200

    refactored the flippers

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    HingeJoint hinge;
+
+    public float restPosition = 0f;
+    public float pressedPosition = 45f;
+    public float hitStrength = 10000f;
+    public float flipperDamper = 150f;
+
+    public string inputName;
+
+    void Start()
+    {
+        hinge = GetComponent<HingeJoint>();
+        hinge.useSpring = true;
+    }
+
+    void Update()
+    {
+        JointSpring spring = new JointSpring();
+        spring.spring = hitStrength;
+        spring.damper = flipperDamper;
+
+        if(Input.GetAxis(inputName) == 1)
+        {
+            spring.targetPosition = pressedPosition;
+        }
+        else
+        {
+            spring.targetPosition = restPosition;
+        }
+
+        hinge.spring = spring;
+        hinge.useLimits = true;
+    }
+}
