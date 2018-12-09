aea8f625d1b5f1b68258986440c5c28b931de235
Raissa Scheeren <raissa.scheeren@cwi.com.br>
2018-12-09 17:37:39 -0200

    added collectables

---
+++ Assets/Scripts/DreamEater.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DreamEater : MonoBehaviour
+{
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.CompareTag("collectable"))
+        {
+            collider.gameObject.SetActive(false);
+        }
+    }
+}
