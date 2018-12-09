2dd72f917035ce2f86293dd2894d4c55c83a6f7a
Raissa Scheeren <raissa.scheeren@cwi.com.br>
2018-12-09 19:49:42 -0200

    randomied collectables spawn point

---
+++ Assets/Scripts/Collectables.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectables : MonoBehaviour
+{
+    public Transform collectable;
+    public float targetTime = 10.0f;
+
+    void Update()
+    {
+        targetTime -= Time.deltaTime;
+
+        if (targetTime <= 0.0f)
+        {
+            timerEnded();
+        }
+    }
+
+    void timerEnded()
+    {
+        float x = Random.Range(-16.0f, -7.0f);
+        float z = Random.Range(-3.0f, 1.0f);
+
+        Instantiate(collectable, new Vector3(x, -1.9f, z), Quaternion.identity);
+
+        targetTime = 10.0f;
+    }
+}
+
