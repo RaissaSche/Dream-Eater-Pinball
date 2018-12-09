52c5ab6951cb5cbac865bca0ac3c64f51f033623
Raissa Scheeren <raissa.scheeren@cwi.com.br>
2018-12-09 18:23:29 -0200

    added score logic

--- Assets/Scripts/DreamEater.cs
+++ Assets/Scripts/DreamEater.cs
@@ -1,14 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DreamEater : MonoBehaviour
 {
+    public Text scoreText;
+    private int score;
+
+    void Start()
+    {
+        score = 0;
+        setScoreText();
+    }
+
     void OnTriggerEnter(Collider collider)
     {
         if (collider.gameObject.CompareTag("collectable"))
         {
             collider.gameObject.SetActive(false);
+            score += 150;
+            setScoreText();
         }
+    }
+
+    void setScoreText()
+    {
+        scoreText.text = "Score: " + score.ToString() + " pts";
     }
 }
