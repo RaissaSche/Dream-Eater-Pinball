a8c876ab8bbd098a71bc78ab874d93109a92ea35
Raissa Scheeren <raissa.scheeren@cwi.com.br>
2018-12-08 22:46:05 -0200

    setting world bounds

---
+++ Assets/Scripts/Border.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Border : MonoBehaviour
+{
+    Rigidbody objRigidBody;
+
+    void Start()
+    {
+        objRigidBody = GetComponent<Rigidbody>();
+    }
+
+    void OnTriggerEnter()
+    {
+        objRigidBody.AddForce(-objRigidBody.velocity * Time.deltaTime);
+    }
+}
